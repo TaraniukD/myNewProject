ccb29ed585cabb9aff126a3e48a24a31d2fd2a14
TaraniukD <taranyk88@ukr.net>
2023-03-14 07:15:10 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { TouchableWithoutFeedback, StyleSheet, View, ImageBackground, Keyboard, require } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet, View, ImageBackground, Keyboard } from 'react-native';
 import RegistrationScreens from './assets/Screens/RegistrationScreens/RegistrationScreens';
 import LoginScreens from './assets/Screens/LoginScreens/LoginScreens';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
 
 export const initialState = {
   login: '',
@@ -15,7 +13,6 @@ export const initialState = {
 export default function App() {
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   const [state, setState] = useState(initialState);
-  const [isReady, setIsReady] = useState(false);
 
   const BgImage = {uri: 'https://pibig.info/uploads/posts/2022-11/1669841130_1-pibig-info-p-altai-oboi-na-telefon-krasivo-1.jpg'};
 
@@ -23,19 +20,6 @@ export default function App() {
     setIsShowKeyboard(false);
     Keyboard.dismiss(); 
   }
-
-  const loadFonts = async () => {
-  await Font.loadAsync({
-    "Roboto-Regular": require("./assets/Fonts/Roboto/Roboto-Regular.ttf"),
-    "Roboto-Bold": require("./assets/Fonts/Roboto/Roboto-Bold.ttf"),
-  });
-  };
-  
-// if (!isReady) {
-//   return (
-//     <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} onError={console.log} />
-//   );
-// }
 
   return (
     <TouchableWithoutFeedback onPress={keyboardHide}>
@@ -50,7 +34,8 @@ export default function App() {
             isShowKeyboard={isShowKeyboard}
             setState={setState}
             state={state}
-            initialState={initialState} /> 
+            initialState={initialState}
+          /> 
           {/* <LoginScreens
           keyboardHide={keyboardHide}
           setIsShowKeyboard={setIsShowKeyboard}
